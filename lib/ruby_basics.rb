e86ffe021e20242aae746c218b1d162a64447b87
Roberto Samia <bojosteph@gmail.com>
2018-09-04 19:11:48 +0000

    automatically backed up by learn

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -4,12 +4,12 @@ def division(num1, num2)
 end
 
 def assign_variable(value = "Bob")
-   puts value
+   puts #{value}
   
 end
 
 def argue(phrase = "I'm right and you are wrong!" )
- puts phrase 
+ puts #{phrase}
 end
 
 def greeting (greeting = "Hi there", name = "Bobby!")
